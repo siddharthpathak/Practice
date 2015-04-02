835b42bec14e1f2be2e5cad964435b1b0f6ed5d1
siddharthpathak <siddharthpathak7@gmail.com>
2015-04-02 17:55:54 +0530

    Create HackerEarth1.cpp
     Link of the problem:
    
     https://www.hackerearth.com/problem/algorithm/flowers-arrangement/

---
+++ HackerEarth1.cpp
@@ -0,0 +1,35 @@
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    long long T, length,i=0;
+    string s;
+    cin>>T;
+    while(T--)
+    {
+    	i=0;
+    	cin>>s;
+    	length=s.size();
+    	while(i<length)
+    	{
+    	if( s[i]=='R' && s[i+1]=='Y' && s[i+2]=='Y')
+    	i=i+3;
+    	else if(s[i]=='R' && s[i+1]=='Y')
+    	i=i+2;
+    	else if(s[i]=='R')
+    	i++;
+    	else 
+    	break;	
+    	}
+    	if(i==length)
+    	cout<<("YES\n");
+    	else
+    	cout<<("NO\n");
+    }
+    
+    return 0;
+}
+
